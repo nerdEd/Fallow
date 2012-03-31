115ed606f8ba897d49c02c6baa2e6eb93c975f08
Ed Schmalzle <ed@nerded.net>
2012-03-31 11:09:45 -0400

    update factory definition syntax

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,32 +1,34 @@
-Factory.define :furrow do |f|
-  f.association :user
-  f.association :seed_user
-  f.duration 5
-end
+FactoryGirl.define do
+  factory :furrow do |f|
+    f.association :user
+    f.association :seed_user
+    f.duration 5
+  end
 
-Factory.define :follow_furrow, :parent => :furrow do |f|
-  f.action 'follow'
-end
+  factory :follow_furrow, :parent => :furrow do |f|
+    f.action 'follow'
+  end
 
-Factory.define :unfollow_furrow, :parent => :furrow do |f|
-  f.action 'unfollow'
-end
+  factory :unfollow_furrow, :parent => :furrow do |f|
+    f.action 'unfollow'
+  end
 
-Factory.define :started_unfollow_furrow, :parent => :furrow do |f|
-  f.state 'started'
-end
+  factory :started_unfollow_furrow, :parent => :furrow do |f|
+    f.state 'started'
+  end
 
-Factory.define :started_follow_furrow, :parent => :follow_furrow do |f|
-  f.state 'started'
-end
+  factory :started_follow_furrow, :parent => :follow_furrow do |f|
+    f.state 'started'
+  end
 
-Factory.define :user do |f|
-  f.sequence(:twitter_id) {|n| "fakeid#{n}"}
-  f.sequence(:nickname) {|n| "frankie0#{n}"}
-  f.token 'my_token'
-  f.secret 'my_secret'
-end
+  factory :user do |f|
+    f.sequence(:twitter_id) {|n| "fakeid#{n}"}
+    f.sequence(:nickname) {|n| "frankie0#{n}"}
+    f.token 'my_token'
+    f.secret 'my_secret'
+  end
 
-Factory.define :seed_user, :parent => :user do |f|
+  factory :seed_user, :parent => :user do |f|
 
+  end
 end
