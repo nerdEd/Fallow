e193212a6d917d87e9c3dad313eaf09488019ab7
Ed Schmalzle <ed@nerded.net>
2012-03-31 11:11:16 -0400

    don't need to set the twitter config in env vars anymore

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Fallow::Application.initialize!
-
-TWITTER_KEY = '3tCxOIJDtxaNlRDiJs56Cg'
-TWITTER_SECRET = 'bDmJerGTU6I8FVh6g9GJK8JX8DX761r4WnXmAeJy0'
