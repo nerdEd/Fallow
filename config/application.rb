a8d500f9b1ed0e7abeb410714dccc0eaa058902e
Ed Schmalzle <ed@nerded.net>
2012-04-18 21:07:41 -0400

    update omniauth config

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,9 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 module Fallow
   class Application < Rails::Application
 
-    config.middleware.use(OmniAuth::Strategies::Twitter, '3tCxOIJDtxaNlRDiJs56Cg', 'bDmJerGTU6I8FVh6g9GJK8JX8DX761r4WnXmAeJy0')
+    config.middleware.use(OmniAuth::Builder) do
+      provider :twitter, '3tCxOIJDtxaNlRDiJs56Cg', 'bDmJerGTU6I8FVh6g9GJK8JX8DX761r4WnXmAeJy0' 
+    end
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
